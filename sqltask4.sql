e51d6b216fe1928d8ca806c9c811cf7a954911f3
Ayush-RajSingh <92213046+Ayush-RajSingh@users.noreply.github.com>
2024-07-03 21:09:46 +0530

    sqltask4

---
+++ sqltask4.sql
@@ -0,0 +1,16 @@
+select * from car_data
+
+select count(car_model) from car_data where price > 60000
+	
+select car_model,comp_name from car_data where comp_name like 'MG' order by car_data ASC limit 10
+
+select max(price), min(price), count(price), avg(price) from car_data where car_model='Ecosport'
+
+select count(car_model) from car_data where car_model='Swift'
+
+select comp_name,car_model from car_data where car_model='Nexon' order by car_data ASC limit 7
+
+SELECT car_model, AVG(price) AS AveragePrice
+FROM car_data
+GROUP BY car_model
+ORDER BY AveragePrice DESC LIMIT 10
